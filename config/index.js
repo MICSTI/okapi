739dc5890c12501e3bc61385a91e1e68d78b6d91
MICSTI <stifter.michael@gmx.net>
2019-06-01 14:44:18 +0200

    Add database connection setup

--- config/index.js
+++ config/index.js
@@ -5,6 +5,13 @@ const config = {
   app: {
     port: process.env.APP_PORT,
   },
+  db: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+  },
 };
 
 module.exports = config;
