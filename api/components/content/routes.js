2432a1f86a18d102de7ece20b06db7502d2d26da
MICSTI <stifter.michael@gmx.net>
2019-08-17 16:06:35 +0200

    feat: return real hash of user's data object

--- api/components/content/routes.js
+++ api/components/content/routes.js
@@ -4,14 +4,15 @@ const userDao = require('../users/dao');
 const errorHandler = require('../../controllers/errorHandler');
 
 router.post('/ping', accessControl.protect, (req, res, next) => {
-  //const hash = userDao.getUserContentHash(req.user.id);
-  const hash = req.user.id;
+  const hash = userDao.getUserContentHash(req.user.id);
 
   if (!hash) {
     return next(errorHandler.createError(400, "No hash available for this user"));
   }
 
-  return res.status(200).send(hash);
+  return res.status(200).json({
+    hash,
+  });
 });
 
 router.post('/update', accessControl.protect, (req, res, next) => {
