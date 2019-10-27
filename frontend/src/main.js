27188e723917941fef825eccdc0742760b07cfcb
MICSTI <stifter.michael@gmx.net>
2019-10-27 16:14:08 +0100

    chore: remove Vuetify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-//import vuetify from '@/plugins/vuetify'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
@@ -11,5 +10,4 @@ new Vue({
   router,
   store,
   render: h => h(App),
-  //vuetify,
 }).$mount('#app')
