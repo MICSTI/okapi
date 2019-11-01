7095ffa80437815880a6b0d718bca4e835996cb0
MICSTI <stifter.michael@gmx.net>
2019-11-01 21:53:34 +0100

    feat: add skeleton for contact detail page

---
+++ frontend/src/views/Contact.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="is-size-4">
+        Martina <span class="has-text-weight-bold">Musterfrau</span>
+      </div>
+    </div>
+
+    <div class="container">
+      <div>
+        <span class="tag is-info">Close friend</span>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="header has-text-weight-bold is-uppercase">Education</div>
+      <div class="content">
+        <ul>
+          <li>
+            <div>10/2016 - 07/2018</div>
+            <div class="has-text-weight-bold">IT & Mobile Security</div>
+            <div>FH JOANNEUM Kapfenberg</div>
+            <div>Master of Science</div>
+          </li>
+          <li>
+            <div>10/2013 - 07/2016</div>
+            <div class="has-text-weight-bold">Internettechnik</div>
+            <div>FH JOANNEUM Kapfenberg</div>
+            <div>Bachelor of Science</div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</template>
